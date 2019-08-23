9bdf372f2d3fec0d4dd2d1cde83df81d5afbd5da
LuchoPonce <luis.ponce@utec.edu.pe>
2019-08-22 22:05:41 -0700

    Final commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,53 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main(){
+
+	unsigned int numInicial,originaInicio, numFinal,originaFinal;
+
+	while ( cin>>numInicial>>numFinal){
+		originaInicio = numInicial;
+		originaFinal = numFinal;
+		if(numInicial>numFinal){
+			swap(numInicial,numFinal);
+		}
+        int variar = numInicial;
+        int maximo=0;
+        while(variar<=numFinal){
+        	unsigned int temporal = variar;
+            int contador = 1;
+            if(buscar[variar]==0){
+                while(temporal!=1){ 
+
+                    if(temporal%2==0){
+                        temporal = temporal/2;
+                    }
+                    else{
+                        temporal = (temporal*3) + 1;
+                    }
+                    contador++;
+                }
+
+                
+               buscar[variar]=contador;
+
+            }
+            else{
+                contador = buscar[variar];
+            }
+            if(contador>maximo){
+                maximo = contador;        
+            }
+            variar++;
+        }
+
+        std::cout<<originaInicio<<" "<<originaFinal<<" "<<maximo<<"\n";
+        
+
+	}
+	
+
+	return 0;
+
 }
